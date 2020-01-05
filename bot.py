dc02f8ca589122341f03dfd52b5de40947e47b62
ColinTheShark <nicolas@neht.xyz>
2020-01-06 00:14:43 +0100

    Multiple Changes: - Re-format start-message - Add greeting when added to groups - Disable preview for /start - Add message handler for group-adds

--- bot.py
+++ bot.py
@@ -4,25 +4,42 @@ from random import randint
 from pyrogram import Client, Emoji, Filters, Message
 
 RE_DICE = re.compile(r"(?P<count>\d*)d(?P<sides>\d+)")
-START = """
-Hi there {} {}
-
-I can roll you any amount of dice with any amount of sides.
-Just send /roll and pass a die format like `2d6` for two six-sided die.
-Maybe just `d4` for a four-sided die. Or even `5d20` for five 20-sided die.
-
-You can also [add me to groups](https://t.me/Rolling_Dice_Bot?startgroup=hi_there) \
-so I can roll you a dice there :D
-"""
+START = (
+    "Hi there {} {}\n\n"
+    + "I can roll you any amount of dice with any amount of sides. "
+    + "Just send /roll and pass a die format like `2d6` for two six-sided die. "
+    + "Maybe just `d4` for a four-sided die. Or even `5d20` for five 20-sided die.\n\n"
+    + "You can also [add me to groups](https://t.me/Rolling_Dice_Bot?startgroup=) "
+    + "so I can roll you a dice there :D"
+)
+START_GROUP = (
+    "Thanks for adding me to your group {0}\n"
+    + "You can either just /roll for a standard six-sided die, or use the [Dice "
+    + "Notation](https://en.wikipedia.org/wiki/Dice_notation#Standard_notation) to "
+    + "roll more die with more sides.\n"
+    + "Use `/roll d10` for a ten-sided dice, or `/roll 4d20` for four 20-sided die. "
+    + "It's that easy {0}"
+)
 DICE = "Your rolled dice " + Emoji.GAME_DIE
 
 # App definition
 app = Client(session_name="DiceBot")
 
 
-@app.on_message(Filters.command("start"))
+@app.on_message(Filters.command("start") & Filters.private)
 def start(app: Client, message: Message):
-    message.reply_text(START.format(message.from_user.first_name, Emoji.GAME_DIE))
+    message.reply_text(
+        START.format(message.from_user.first_name, Emoji.GAME_DIE),
+        disable_web_page_preview=True,
+    )
+
+
+@app.on_message(Filters.new_chat_members)
+def added(app: Client, message: Message):
+    if app.get_me().id in [i.id for i in message.new_chat_members]:
+        message.reply_text(
+            START_GROUP.format(Emoji.GAME_DIE), disable_web_page_preview=True
+        )
 
 
 @app.on_message(Filters.command("roll") & ~Filters.edited)
